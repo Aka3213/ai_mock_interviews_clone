df9a1156914d8ae61679a72a591aca070ffcb861
Aka3213 <akashs23csec32@gmail.com>
2025-07-25 22:57:42 +0530

    Initial commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,12 @@
+import postcss from "postcss";
+
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-export default config;
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
